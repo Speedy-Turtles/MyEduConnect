ccc69f45176f9430908f2df8afdf69b1a54b6e32
talel <talelmejri8@gmail.com>
2023-03-21 01:16:52 +0100

    add classe mfs

---
+++ laravel/database/migrations/2014_10_11_232653_create_classes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->String("nom");
+            $table->foreignId("specialite_id");
+            $table->foreign("specialite_id")->references("id")->on("specialites")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
