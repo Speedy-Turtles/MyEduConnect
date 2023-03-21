fc2147ff55c38dfc57b388d023d726c7fdc91462
talel <talelmejri8@gmail.com>
2023-03-21 01:16:22 +0100

    clear default

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
     /**
      * Register the commands for the application.
      */
+
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
