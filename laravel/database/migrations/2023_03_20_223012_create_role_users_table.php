32a0ddf8da1773e11244ddf4613903df5c4daa5d
talel <talelmejri8@gmail.com>
2023-03-21 01:18:43 +0100

    add roleuser mfs

---
+++ laravel/database/migrations/2023_03_20_223012_create_role_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_users', function (Blueprint $table) {
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("role_id");
+            $table->foreign("user_id")->references('id')->on("users")->onDelete("cascade");
+            $table->foreign("role_id")->references('id')->on("roles")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_users');
+    }
+};
