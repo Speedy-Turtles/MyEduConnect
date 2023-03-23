c68eb50d7c5575eb60f8a0b247bb8c201950d7c8
talel <talelmejri8@gmail.com>
2023-03-23 17:04:54 +0100

    add register Controller

---
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\RoleUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function Signup(Request $request)
+    {
+
+        if ($request->hasFile('photo')) {
+            $file_name = time() . '_' . $request->photo->getClientOriginalName();
+            $image=$request->file('photo')->storeAs('users',$file_name,'public');
+            $image_name='/storage'.$image;
+        }else{
+            $image_name=$request->firstname[0].''.$request->lastname[0];
+        }
+
+        $user=User::create([
+            "LastName"=>$request->lastname,
+            "FirstName"=>$request->firstname,
+            "email"=>$request->email,
+            "password"=>bcrypt($request->password),
+            "Cin"=>$request->cin,
+            "sex"=>$request->sex,
+            "photo"=>$image_name,
+            "Birth_day"=>$request->birth_day,
+            "classe_id"=>$request->classe_id
+        ]);
+
+
+        $role=new Role();
+        $role->Role_name=$request->Role_name;
+        $role->save();
+
+        $role_user=new RoleUser();
+        $role_user->user_id=$user->id;
+        $role_user->role_id=$role->id;
+        $role_user->save();
+
+        return response()->json(['data'=>"user created"],200);
+    }
+}
