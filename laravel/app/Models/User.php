78b052f4af01e58c2ce09c34d800dc849388c87e
talel <talelmejri8@gmail.com>
2023-03-24 17:15:44 +0100

    tools

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\VerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
