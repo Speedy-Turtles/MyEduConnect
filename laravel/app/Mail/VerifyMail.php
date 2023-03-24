5103c02c3c517f865c769378c492098ed409a0c3
talel <talelmejri8@gmail.com>
2023-03-24 17:15:28 +0100

    add mail

---
+++ laravel/app/Mail/VerifyMail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $user;
+    public function __construct($user)
+    {
+        $this->user=$user;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Verify Mail',
+        );
+    }
+
+      /**
+     * Get the build
+     */
+    public function build()
+    {
+        return $this->subject('Link Verify Mail')
+                     ->markdown('EmailVerification');
+    }
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
