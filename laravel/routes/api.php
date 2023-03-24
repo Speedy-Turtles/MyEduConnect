538eba9bc13ede510fa3a9a922084aa3a114e460
talel <talelmejri8@gmail.com>
2023-03-24 17:16:36 +0100

    add api renvoyerlink,verifyemail

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(["prefix"=>"/auth"],function(){
     Route::post('/signup',[RegisterController::class,'Signup']);
+    Route::get('/verifyEmail/{email}',[VerifyEmailController::class,'VerifyEmail']);
+    Route::get("/RenvoyerLink/{email}",[VerifyEmailController::class,'renvoyer']);
 });
+
