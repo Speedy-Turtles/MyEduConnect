fa8dfd28aea5f9957045ea14e5346bbafb55d5a7
hkimi amin <hkimiamin02@gmail.com>
2023-03-26 04:13:39 +0100

    add dashboard

---
+++ vue/src/views/chef_departement/dashboardView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <navbar/>
+        <navigationDrawer/>
+</div>
+</template>
+<script>
+import navbar from '@/components/chef_departement/navbar.vue';
+import navigationDrawer from '@/components/chef_departement/navigationDrawer.vue';
+    export default{
+        components:{
+            navbar,
+            navigationDrawer
+        }
+    }
+</script>
+<style>
+</style>
