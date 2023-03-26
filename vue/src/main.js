bd4e765f9dfc2573d91d2bcc791625735c24875f
talel <talelmejri8@gmail.com>
2023-03-26 02:59:43 +0100

    fix vuelidate

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,12 +4,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Vuelidate from "./plugins/vuealidate";
 import 'aos/dist/aos.css';
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  Vuelidate,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
