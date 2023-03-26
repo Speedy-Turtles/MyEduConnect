06228abe777d8130995f84c2fdfce4d207a215ea
hkimi amin <hkimiamin02@gmail.com>
2023-03-26 04:18:04 +0100

    fix bugs

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class,"role_users");
     }
 
-    public function norifications(){
+    public function notifications(){
         return $this->hasMany(Notification::class);
     }
 
