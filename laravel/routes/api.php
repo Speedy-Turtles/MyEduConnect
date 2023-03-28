831172b18f2b0453aefec4e6e39f83a43bb9f2d0
ghassen <ghassenahmed74@gmail.com>
2023-03-28 03:42:36 +0100

    create route group:/documents + add route get allDocuments

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Document\DocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=>'/documents'],function(){
+    Route::get('AllDocuments',[DocumentController::class,'AllDocuments']);
+});
+
