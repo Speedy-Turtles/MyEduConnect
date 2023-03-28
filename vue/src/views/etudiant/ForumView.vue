e23e6e55fa3395a20c6a24abc5ec1d76be610c85
ghassen <ghassenahmed74@gmail.com>
2023-03-28 01:59:41 +0100

    add forum view

---
+++ vue/src/views/etudiant/ForumView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="forum">
+
+            <Navbar></Navbar>
+            <div class="main">
+               <h1>forum</h1>
+            </div>
+            <Footer></Footer>
+    </div>
+</template>
+<script>
+import Navbar from '@/components/etudiant/layout/Navbar.vue'
+import Footer from '@/components/etudiant/layout/Footer.vue'
+export default {
+    components:{
+        Navbar,Footer
+    }
+}
+</script>
+<style>
+.main{
+   height: auto;
+}
+</style>
