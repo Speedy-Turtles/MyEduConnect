9e8e7b4f8250638a07e8e07321e7295e0e318b5b
talel <talelmejri8@gmail.com>
2023-03-29 02:45:22 +0100

    get niveau

--- laravel/app/Http/Controllers/InfoUserController.php
+++ laravel/app/Http/Controllers/InfoUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Classe;
 use App\Models\Role;
 use App\Models\RoleUser;
 use App\Models\User;
@@ -37,6 +38,25 @@ class InfoUserController extends Controller
                 "success"=>true
             ]);
         }
+    }
+
+    public function GetNiveauUser(Request $request){
+        $user=User::where("id",$request->user()->id)->first();
+        $classe=Classe::where("id",$user->classe_id)->with("Specialite")->first();
+        return response()->json([
+            "data"=>$classe,
+        ],200);
+        /*if($user){
+            return response()->json([
+                "data"=>"Exist",
+                "success"=>false
+            ],201);
+        }else{
+            return response()->json([
+                "data"=>"not Exist",
+                "success"=>true
+            ]);
+        }*/
     }
 
     public function ActiveUser($id){
