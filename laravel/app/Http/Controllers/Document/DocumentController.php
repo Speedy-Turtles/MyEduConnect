9b6aa263882e56b8daff2d75fc6fe4a93dcd205a
ghassen <ghassenahmed74@gmail.com>
2023-03-29 01:44:49 +0100

    add getDocument

--- laravel/app/Http/Controllers/Document/DocumentController.php
+++ laravel/app/Http/Controllers/Document/DocumentController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Document;
 
 use App\Http\Controllers\Controller;
 use App\Models\Document;
+use App\Models\Demande;
 use Illuminate\Http\Request;
 
 class DocumentController extends Controller
 {
     public function AllDocuments(){
         $documents=Document::get();
-        return response()->json($documents,200);
+        return response()->json($documents);
     }
 }
