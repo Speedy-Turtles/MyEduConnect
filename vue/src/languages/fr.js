93fe39c0575623eac1bcc737712be599676664d3
talel <talelmejri8@gmail.com>
2023-03-31 01:29:29 +0100

    traduction french

---
+++ vue/src/languages/fr.js
@@ -0,0 +1,41 @@
+export default{
+    data(){
+        return{
+            fr:{
+                "الجمهورية":"Tunsienne",
+                "وزارة":"Wiazara",
+                "الادارة":"Idara ",
+                "المعهد":"ma3hed",
+                "شهادة":"attestation de présence",
+                "يشهد":"yachhadou",
+                "الطالبة":"etudiante",
+                "الطالب":"etudiant",
+                "الاسم":"Prenom",
+                "اللقب":"Nom",
+                "المولود":"mawlouuud",
+                "ب  تونس":"dans tunisie",
+                "بطاقة":"carte",
+                "مرسم":"presnete",
+                "مرسمة":"prnetee",
+                "بالسنة":"annee",
+                "الفوج":"classe",
+                "الاجازة":"atettststst",
+                "الاختصاص":"specccc",
+                "رقم":"Numero",
+                "تواصل":"macrehee",
+                "بانتضام":"bintidham ",
+                "سلمت":"takee",
+                "المعني":"sdd",
+                "دراسته":"sds",
+                "دراستها":"dsds",
+                "المعنية":"ddjddhdjh",
+                "بالامر":"bil2amer",
+                "بنزرت":"benzert",
+                'الكاتب':" el kateb",
+                "كمال":"kamel el karoui",
+                "مركب":" 65",
+                "الهاتف":"email fax phone"
+            }
+        }
+    }
+}
