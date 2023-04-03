4380a1ef44bcfa202e50acb7a23b62c09b781bd5
hkimi amin <hkimiamin02@gmail.com>
2023-04-03 14:52:56 +0100

    clear default

--- vue/src/service/AuthService/Auth.js
+++ vue/src/service/AuthService/Auth.js
@@ -1,56 +1,56 @@
 import axios from "axios";
-import  "@/plugins/axios";
-import {AuthUser} from "@/store/Store.js";
+import "@/plugins/axios";
+import { AuthUser } from "@/store/Store.js";
 
-export default{
+export default {
 
-    StoreUser(user){
-        let data=new FormData();
-        data.append("firstname",user.firstname);
-        data.append("lastname",user.lastname);
-        data.append("email",user.email);
-        data.append("birth_day",user.birthday);
-        data.append("password",user.password);
-        data.append("Role_name",user.Role_name);
-        data.append("photo",user.photo);
-        data.append("sex",user.sex);
-        data.append("cin",user.cin);
-        data.append("num_tlf",user.num_tlf);
-        data.append("classe_id",user.classe_id);
-        const config={
-            Headers:{
-                "content-type":"multipart/form-data",
+    StoreUser(user) {
+        let data = new FormData();
+        data.append("firstname", user.firstname);
+        data.append("lastname", user.lastname);
+        data.append("email", user.email);
+        data.append("birth_day", user.birthday);
+        data.append("password", user.password);
+        data.append("Role_name", user.Role_name);
+        data.append("photo", user.photo);
+        data.append("sex", user.sex);
+        data.append("cin", user.cin);
+        data.append("num_tlf", user.num_tlf);
+        data.append("classe_id", user.classe_id);
+        const config = {
+            Headers: {
+                "content-type": "multipart/form-data",
             },
         };
         console.log(user);
-        return axios.post("auth/signup",data,config)
+        return axios.post("auth/signup", data, config)
     },
 
-    async login(email,password){
-        const store=AuthUser();
-        const res=await axios.post("auth/login",{email,password});
-        const data=res.data.data;
-       // console.log(data);
-        if(res.status==200){
-            store.login(data.token,data.user,data.Isadmin,
-                data.Istechnicien,data.Isetudiant,data.IsChefDepartement
-                );
-        }else{
+    async login(email, password) {
+        const store = AuthUser();
+        const res = await axios.post("auth/login", { email, password });
+        const data = res.data.data;
+        // console.log(data);
+        if (res.status == 200) {
+            store.login(data.token, data.user, data.Isadmin,
+                data.Istechnicien, data.Isetudiant, data.IsChefDepartement
+            );
+        } else {
             store.logout();
         }
     },
 
-    RenvoyerEmail(email){
-        return axios.get("auth/RenvoyerLink/"+email);
+    RenvoyerEmail(email) {
+        return axios.get("auth/RenvoyerLink/" + email);
     },
-    VerifyEmail(email){
-        return axios.get("auth/verifyEmail/"+email);
+    VerifyEmail(email) {
+        return axios.get("auth/verifyEmail/" + email);
     },
-    forgotPassword(email){
-        return axios.post("auth/forgotPassword/"+email);
+    forgotPassword(email) {
+        return axios.post("auth/forgotPassword/" + email);
     },
-    changerPassword(data){
-        return axios.post("auth/ChangerPassword",data);
+    changerPassword(data) {
+        return axios.post("auth/ChangerPassword", data);
     }
 
 }
