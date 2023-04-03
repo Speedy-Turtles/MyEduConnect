367f3bc9a9f16de551007e3cb564303b34b59938
talel <talelmejri8@gmail.com>
2023-04-03 21:56:34 +0100

    add demande

--- laravel/app/Http/Controllers/Document/DocumentController.php
+++ laravel/app/Http/Controllers/Document/DocumentController.php
@@ -11,6 +11,21 @@ class DocumentController extends Controller
 {
     public function AllDocuments(){
         $documents=Document::get();
-        return response()->json($documents);
+        return response()->json(['data'=>$documents],200);
+    }
+
+    public function addDemande(Request $request){
+        $demande=new Demande();
+        $demande->user_id=$request->user()->id;
+        $demande->document_id=$request->document_id;
+        $demande->etat=0;
+        $demande->nombre=0;
+        $demande->Langue=$request->langue;
+        $demande->save();
+        return response()->json(['data'=>$demande],201);
+        /*$demande=Demande::create([
+
+        ]);*/
+
     }
 }
