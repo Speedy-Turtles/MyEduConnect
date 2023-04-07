c451d7d75e0153023feaa7db5bbed86b56813534
NaamenMalek <malek.naamen@gmail.com>
2023-04-07 10:26:14 +0100

    add footer

---
+++ vue/src/components/home/Footer.vue
@@ -0,0 +1,47 @@
+
+<template>
+    <v-footer color="#525fe1" class="white--text text-center mb-0">
+        <v-row>
+            <v-col cols="3">
+                <v-toolbar-title class="ms-5">
+                    <img src="../../../public/images/logo-no-background-white.svg" alt="" width="150px" srcset="">
+                </v-toolbar-title>
+            </v-col>
+            <v-col cols="3">
+                <p class="font-weight-bold display-5">
+                    Useful Links
+                </p>
+            </v-col>
+            <v-col cols="3">
+                <p class="font-weight-bold display-5">
+                    Resources
+                </p>
+            </v-col>
+            <v-col cols="3"> <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn></v-col>
+
+            <br />
+
+            <v-col>{{ new Date().getFullYear() }} — <strong>MYEDUCONNECT</strong> </v-col> </v-row>
+    </v-footer>
+</template>
+<script>
+export default {
+    data: () => ({
+        icons: [
+            'mdi-facebook',
+            'mdi-twitter',
+            'mdi-linkedin',
+            'mdi-instagram',
+        ],
+    }),
+}
+</script>
