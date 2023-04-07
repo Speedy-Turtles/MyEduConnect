997846a93dae5e00e9f4ff6443aa3867c0db410b
NaamenMalek <malek.naamen@gmail.com>
2023-04-07 10:26:06 +0100

    add main content

---
+++ vue/src/components/home/Content.vue
@@ -0,0 +1,167 @@
+<template>
+    <div>
+        <v-container fluid class="mt-5">
+            <div class="pa-4 text-center rounded-0">
+                <v-row>
+                    <v-col class="align-self-center" cols="6">
+                        <p class="d-flex align-self-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quibusdam voluptatum facere,
+                            incidunt, odit laboriosam eum ipsa dicta tenetur asperiores maiores similique nesciunt neque
+                            quia dolor optio cupiditate. Ut, reiciendis.</p>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-img src="../../../public/images/person-studying.png" contain height="100%"></v-img>
+
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+
+        <v-container style="background-color: #525fe1;" class="text-center" fluid>
+            <div class="pa-4 text-center rounded-0">
+                <v-row>
+                    <v-col cols="3">
+                        <p>1</p>
+                    </v-col>
+                    <v-col cols="3">
+                        <p>1</p>
+
+                    </v-col>
+                    <v-col cols="3">
+                        <p>1</p>
+
+                    </v-col>
+                    <v-col cols="3">
+                        <p>1</p>
+
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+
+        <v-container class="pa-4">
+            <div>
+                <v-row>
+                    <v-col cols="6">
+                        <v-card class="mx-auto" max-width="344">
+                            <v-card-text>
+                                <p class="text-h4 text--primary">
+                                    el·ee·mos·y·nar·y
+                                </p>
+                                <p>adjective</p>
+                                <div class="text--primary">
+                                    relating to or dependent on charity; charitable.<br>
+                                    "an eleemosynary educational institution."
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-card class="mx-auto" max-width="344">
+                            <v-card-text>
+                                <p class="text-h4 text--primary">
+                                    el·ee·mos·y·nar·y
+                                </p>
+                                <p>adjective</p>
+                                <div class="text--primary">
+                                    relating to or dependent on charity; charitable.<br>
+                                    "an eleemosynary educational institution."
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="6">
+                        <v-card class="mx-auto" max-width="344">
+                            <v-card-text>
+                                <p class="text-h4 text--primary">
+                                    el·ee·mos·y·nar·y
+                                </p>
+                                <p>adjective</p>
+                                <div class="text--primary">
+                                    relating to or dependent on charity; charitable.<br>
+                                    "an eleemosynary educational institution."
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-card class="mx-auto" max-width="344">
+                            <v-card-text>
+                                <p class="text-h4 text--primary">
+                                    el·ee·mos·y·nar·y
+                                </p>
+                                <p>adjective</p>
+                                <div class="text--primary">
+                                    relating to or dependent on charity; charitable.<br>
+                                    "an eleemosynary educational institution."
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+
+        <v-container fluid>
+            <div class="pa-4 text-center rounded-0">
+                <v-row>
+                    <v-col cols="6">
+                        <v-img src="../../../public/images/business.png" contain height="100%"></v-img>
+
+
+                    </v-col>
+                    <v-col class="align-self-center" cols="6">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quibusdam voluptatum facere,
+                            incidunt, odit laboriosam eum ipsa dicta tenetur asperiores maiores similique nesciunt neque
+                            quia dolor optio cupiditate. Ut, reiciendis.</p>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+        <v-container>
+            <div class="pa-4 text-center rounded-0">
+                <v-row>
+                    <v-col cols="3">
+                        <v-avatar size="150">
+                            <v-img src="../../../public/images/image2.jpeg" contain></v-img>
+                        </v-avatar>
+                        <p>nom&prenom</p>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-avatar size="150">
+                            <v-img src="../../../public/images/image2.jpeg" contain></v-img>
+                        </v-avatar>
+                        <p>nom&prenom</p>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-avatar size="150">
+                            <v-img src="../../../public/images/image2.jpeg" contain></v-img>
+                        </v-avatar>
+                        <p>nom&prenom</p>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-avatar size="150">
+                            <v-img src="../../../public/images/image2.jpeg" contain></v-img>
+                        </v-avatar>
+                        <p>nom&prenom</p>
+
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    data: () => ({
+        //
+
+    }),
+    components: {
+    },
+}
+</script>
