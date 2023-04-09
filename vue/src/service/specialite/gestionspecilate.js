47f7488878ae0161a26f6dc2c15f35c48a351ac0
hkimi amin <hkimiamin02@gmail.com>
2023-04-09 05:58:17 +0100

    add update front end

--- vue/src/service/specialite/gestionspecilate.js
+++ vue/src/service/specialite/gestionspecilate.js
@@ -7,5 +7,14 @@ export default {
     },
     addSpecialite(specialite) {
         return axios.post("specialte/addSpecialite", specialite);
+    },
+    deleteSpecialite(id) {
+        return axios.delete("specialte/deleteSpecialite/" + id);
+    },
+    getSpecialiteByid(id) {
+        return axios.get("specialte/getSpecialiteById/" + id);
+    },
+    updateSpecialite(id, specialite) {
+        return axios.put("specialte/updateSpecialite/" + id, specialite);
     }
 }
--- vue/src/views/chef_departement/gererSpecialiteView.vue
+++ vue/src/views/chef_departement/gererSpecialiteView.vue
@@ -3,6 +3,10 @@
         <navbar />
         <navigationDrawer :current-page="cureentPage" />
         <div class="main_content">
+            <v-row> <v-col>
+                    <v-btn @click="currentProf = 'add'" color="#3f51b5" class="float-right"
+                        text-color="white"><v-icon>mdi-plus</v-icon></v-btn>
+                </v-col></v-row>
             <v-row>
                 <v-col align="center" justify="center">
                     <v-btn v-if="currentProf == 'list'" text :loading="loader" disabled color="blue-grey"
@@ -10,6 +14,7 @@
                     </v-btn>
                 </v-col>
             </v-row>
+
             <v-container v-if="Specialites.length == 0 && currentProf == 'list'">
                 <v-row align="center" justify="center">
                     <h1 style="text-align=center;color: #3f51b5;">Oops ! there's no data available</h1>
@@ -51,16 +56,15 @@
                                 <td>
                                     <v-row>
                                         <v-col>
-                                            <v-btn color="#3f51b5"
-                                                :loading="loaderAcceptBtn"><v-icon>mdi-pen</v-icon></v-btn>
+                                            <v-btn color="#3f51b5" :loading="loaderAcceptBtn"
+                                                @click="getSpecialite(Specialite.id)"><v-icon>mdi-pen</v-icon></v-btn>
                                         </v-col>
                                         <v-col>
                                             <v-btn color="#ffcf59"
-                                                :loading="loaderRefuseBtn"><v-icon>mdi-delete</v-icon></v-btn>
+                                                @click="dialog = true; deleteId = Specialite.id"><v-icon>mdi-delete</v-icon></v-btn>
                                         </v-col>
                                         <v-col>
-                                            <v-btn color="#a6cd49"
-                                                :loading="loaderRefuseBtn"><v-icon>mdi-information-outline</v-icon></v-btn>
+                                            <v-btn color="#a6cd49"><v-icon>mdi-information-outline</v-icon></v-btn>
                                         </v-col>
                                     </v-row>
                                 </td>
@@ -83,7 +87,30 @@
                                     <v-text-field v-model="name" :counter="10" :rules="nameRules" required></v-text-field>
                                     <v-label>niveau</v-label>
                                     <v-select :items="niveaux" v-model="niveau" :rules="niveauRules" required></v-select>
-                                    <v-btn type="submit" class="float-right" color="#525fe1" :disabled="!valid" :loading="AddLoader">add a speciality</v-btn>
+                                    <v-btn type="submit" class="float-right" color="#525fe1" :disabled="!valid"
+                                        :loading="AddLoader">add a speciality</v-btn>
+                                </v-form>
+                            </v-sheet>
+                        </v-card-body>
+                    </v-card>
+                </template>
+            </div>
+            <div class="add-specialite" v-if="currentProf == 'update'">
+                <template>
+                    <v-card elavation="4" height="450">
+                        <v-card-title class="text-center">update speciality</v-card-title>
+                        <v-card-body>
+                            <v-sheet width="500" class="mx-auto">
+                                <v-form validate-on="submit" method="post" ref="form2" v-model="valid2"
+                                    @submit.prevent="updateSpeciality()">
+                                    <v-label>speciaity name</v-label>
+                                    <v-text-field v-model="editSpecailite.type" :counter="10" :rules="nameRules"
+                                        required></v-text-field>
+                                    <v-label>niveau</v-label>
+                                    <v-select :items="niveaux" v-model="editSpecailite.niveau" :rules="niveauRules"
+                                        required></v-select>
+                                    <v-btn type="submit" class="float-right" color="#525fe1" :disabled="!valid2"
+                                        :loading="loaderUpdate">update speciality</v-btn>
                                 </v-form>
                             </v-sheet>
                         </v-card-body>
@@ -108,12 +135,35 @@
                 </v-btn>
             </template>
         </v-snackbar>
+        <div class="confirm">
+            <template>
+                <v-row justify="center">
+                    <v-dialog v-model="dialog" max-width="500">
+                        <v-card>
+                            <v-card-title class="text-h5">
+                                are you sure you want to delete this speciality
+                            </v-card-title>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="red darken-1" text @click="deleteSpeciality()" :loading="loaderRefuseBtn">
+                                    Delete
+                                </v-btn>
+                                <v-btn color=" darken-1" text @click="dialog = false">
+                                    cancel
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </v-row>
+            </template>
+        </div>
     </div>
 </template>
 <script>
 import navbar from '@/components/chef_departement/navbar.vue';
 import navigationDrawer from '@/components/chef_departement/navigationDrawer.vue';
 import gestionspecialite from '@/service/specialite/gestionspecilate'
+
 export default {
     components: {
         navbar,
@@ -124,17 +174,24 @@ export default {
     },
     data() {
         return {
-            valid:true,
+            valid2: false,
+            deleteId: null,
+            loaderUpdate:false,
+            valid: true,
+            dialog: false,
             cureentPage: "specialite",
             Specialites: [],
             loader: false,
             snackbar: false,
+            editSpecailite: null,
             text: '',
+            edit: false,
             timeout: 2000,
             color: '',
             loaderAcceptBtn: false,
             loaderRefuseBtn: false,
             page: 1,
+            search: '',
             currentProf: "list",
             niveaux: ['tronc commun', 'deuxieme anne', 'troisieme anne'],
             nameRules: [
@@ -144,9 +201,9 @@ export default {
             niveauRules: [
                 v => !!v || 'Niveau is required',
             ],
-            name:'',
-            niveau:null,
-            AddLoader:false,
+            name: '',
+            niveau: null,
+            AddLoader: false,
         }
     },
     methods: {
@@ -159,38 +216,109 @@ export default {
             })
         },
         addSpeciality() {
-            this.AddLoader=true;
+            this.AddLoader = true;
             this.$refs.form.validate();
-            if(this.valid==true){
-            if(this.niveau=="tronc commun"){
-                this.niveau=1;
-            }else if(this.niveau=='deuxieme anne'){
-                this.niveau=2;
-            }else{
-                this.niveau=3;
+            if (this.valid == true) {
+                if (this.niveau == "tronc commun") {
+                    this.niveau = 1;
+                } else if (this.niveau == 'deuxieme anne') {
+                    this.niveau = 2;
+                } else {
+                    this.niveau = 3;
+                }
+                let specialite = {
+                    "type": this.name,
+                    "niveau": this.niveau
+                }
+                gestionspecialite.addSpecialite(specialite).then(response => {
+                    this.AddLoader = false;
+                    this.snackbar = true;
+                    this.text = "speciality added succesfully";
+                    this.color = "green";
+                    this.name = '';
+                    this.niveau = null;
+                    this.currentProf = "list";
+                    this.getAllSpecialite();
+                }).catch((error) => {
+                    this.AddLoader = false;
+                    this.snackbar = true;
+                    this.text = "there's was an error submitting your request";
+                    this.color = "red";
+                })
             }
-            let specialite={
-                "type":this.name,
-                "niveau":this.niveau
-            }
-            gestionspecialite.addSpecialite(specialite).then(response=>{
-                this.AddLoader=false;
-                this.snackbar=true;
-                this.text="speciality added succesfully";
-                this.color="green";
-                this.name='';
-                this.niveau=null;
-                this.currentProf="list";
+        },
+        deleteSpeciality() {
+            this.loaderRefuseBtn = true;
+            gestionspecialite.deleteSpecialite(this.deleteId).then(response => {
+                console.log(response.data);
+                this.loaderRefuseBtn = false;
+                this.dialog = false;
+                this.snackbar = true;
+                this.text = "specialite deleted succesfully";
+                this.color = "green";
+                this.deleteId = null;
                 this.getAllSpecialite();
-            }).catch((error)=>{
-                this.snackbar=true;
-                this.text="there's was an error submitting your request";
-                this.color="red";
+            }).catch((error) => {
+                this.loaderRefuseBtn = false;
+                this.dialog = false;
+                this.snackbar = true;
+                this.deleteId = null;
+                this.text = "there's was an error submitting your request";
+                this.color = "red";
             })
+        },
+        getSpecialite(id) {
+            this.loaderAcceptBtn = true;
+            gestionspecialite.getSpecialiteByid(id).then(response => {
+                this.editSpecailite = response.data.data;
+                console.log(this.editSpecailite);
+                if (this.editSpecailite.niveau == 1) {
+                    console.log("aa");
+                    this.editSpecailite.niveau = "tronc commun";
+                } else if (this.editSpecailite.niveau == 2) {
+                    console.log("aa");
+                    this.editSpecailite.niveau = 'deuxieme anne';
+                } else {
+                    console.log("aa");
+                    this.editSpecailite.niveau = 'troisieme anne';
+                }
+                this.loaderAcceptBtn = false;
+                this.currentProf = "update";
+            }).catch((error) => {
+                this.snackbar = true;
+                this.text = "there's was an error submitting your request";
+                this.color = "red";
+            })
+        },
+        updateSpeciality() {
+            this.loaderUpdate = true;
+            this.$refs.form2.validate();
+            if (this.valid2 == true) {
+                if (this.editSpecailite.niveau == "tronc commun") {
+                    this.editSpecailite.niveau = 1;
+                } else if (this.editSpecailite.niveau == 'deuxieme anne') {
+                    this.editSpecailite.niveau = 2;
+                } else {
+                    this.editSpecailite.niveau = 3;
+                }
+                gestionspecialite.updateSpecialite(this.editSpecailite.id, this.editSpecailite).then(response => {
+                    this.loaderUpdate=false;
+                    this.currentProf = "list";
+                    this.snackbar = true;
+                    this.text = "speciality updated succesfully";
+                    this.color="green";
+                    this.editSpecailite=null;
+                    this.getAllSpecialite();
+                }).then((error)=>{
+                    this.snackbar=true;
+                    this.loaderUpdate=false;
+                    this.text="there was a problem submiting your request";
+                    this.color="red";
+                })
             }
         }
-        },
-    }
+    },
+}
 </script>
 <style scoped>
 .main_content {
@@ -202,10 +330,13 @@ export default {
     margin-bottom: 12% !important;
 }
 
+.confirm {
+    z-index: 800000000 !important;
+}
+
 @media screen and (max-width:768px) {
     .main_content {
         margin-left: 5%;
         margin-top: 10%;
     }
-}
-</style>
+}</style>
