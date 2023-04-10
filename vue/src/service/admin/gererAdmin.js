fce47a51330a0a0bbf713379d499d58939067e0e
talel <talelmejri8@gmail.com>
2023-04-10 23:51:30 +0100

    add functions in admin service

--- vue/src/service/admin/gererAdmin.js
+++ vue/src/service/admin/gererAdmin.js
@@ -4,5 +4,12 @@ import  "@/plugins/axios";
 export default{
   getStat(){
     return axios.get("/admin/getstat");
-  }
+  },
+  startVote(data){
+    return axios.post("/admin/StartVote",data);
+  },
+  test_Vote(){
+    return axios.get("/admin/test_Vote");
+  },
+ 
 }
