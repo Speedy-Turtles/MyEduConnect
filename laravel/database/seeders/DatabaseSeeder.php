bf0694f65b2514d7cf6ea097d062eed3d57a0ec2
talel <talelmejri8@gmail.com>
2023-04-10 23:49:56 +0100

    make user Seeder

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        $this->call([
+            UserSeeder::class
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
