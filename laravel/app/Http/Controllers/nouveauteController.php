91160bbb078f565c80d47ea8b93929bf932c2fdc
hkimi amin <hkimiamin02@gmail.com>
2023-04-11 13:25:35 +0100

    add delete add neauvtly

--- laravel/app/Http/Controllers/nouveauteController.php
+++ laravel/app/Http/Controllers/nouveauteController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\nouveaute;
 use Illuminate\Http\Request;
 
 class nouveauteController extends Controller
 {
-
+    public function getAllNouveaute()
+    {
+        $nouveautes = nouveaute::all();
+        if (!empty($nouveautes)) {
+            return response()->json(["data" => $nouveautes], 200);
+        }
+        return response()->json(["data" => []], 404);
+    }
+    public function addnouveaute(Request $request)
+    {
+        $imagePath = public_path($request->photo);
+        $imageData = file_get_contents($imagePath);
+        $photo = base64_encode($imageData);
+        $nouveaute = nouveaute::create([
+            "titre" => $request->titre,
+            "descripition" => $request->descripition,
+            "photo"=>$photo,
+        ]);
+        $nouveaute->save();
+        if ($nouveaute == null) {
+            return response()->json(["data" => null], 400);
+        } else {
+            return response()->json(["data" => $nouveaute], 201);
+        }
+    }
+    public function deletenouveaute($id){
+        $nouveaute=nouveaute::find($id);
+        if($nouveaute!=null){
+            $nouveaute->delete();
+            return response()->json(["data"=>"neveautly deleted succesfully"],200);
+        }
+        return response()->json(["data"=>"neveautly not found"],404);
+    }
 }
