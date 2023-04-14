a26450d5a1ce88ef987f2f19076e6d7a331117da
hkimi amin <hkimiamin02@gmail.com>
2023-04-14 03:49:57 +0100

    add services neauveute

---
+++ vue/src/service/neavaute/gererNeavaute.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import "@/plugins/axios";
+
+export default {
+    getNeavaute() {
+        return axios.get('/nouveaute/Allnouveaute');
+    },
+    addNeavaute(nouveaute) {
+        return axios.post('/nouveaute/addnouveaute', nouveaute);
+    },
+    DeleteNeauv(id) {
+        return axios.delete('/nouveaute/deletenouveaute/' + id);
+    },
+    getNeavauteById(id) {
+        return axios.get('/nouveaute/getnouveauteById/' + id);
+    },
+    updateNeavaute(id, nouveaute) {
+        return axios.put('/nouveaute/updatenouveaute/' + id, nouveaute);
+    }
+}
