0936b307b129706b24e1b950e4c80b0c4bba9789
ghassen <ghassenahmed74@gmail.com>
2023-04-27 01:16:16 +0100

    add emploi model

--- laravel/app/Models/Emploi.php
+++ laravel/app/Models/Emploi.php
@@ -9,4 +9,10 @@ class Emploi extends Model
 {
     use HasFactory;
     protected $guarded=[];
+    public function salle(){
+        return $this->belongsTo(Salle::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
