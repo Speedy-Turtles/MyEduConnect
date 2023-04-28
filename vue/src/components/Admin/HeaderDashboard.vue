4b4c220ae3695ed3b6faf1a3d60c8b448f1a0aa3
talel <talelmejri8@gmail.com>
2023-04-29 00:39:55 +0100

    add notif

--- vue/src/components/Admin/HeaderDashboard.vue
+++ vue/src/components/Admin/HeaderDashboard.vue
@@ -14,25 +14,49 @@
                 }}
               </h2>
               <v-spacer></v-spacer>
-              <v-menu offset-y>
-                <template v-slot:activator="{ on, attrs }">
-                 <v-btn
-                 plain
-                 v-bind="attrs"
-                 v-on="on"
-                 > 
-                   <v-badge content="4" color="green">
-                    <v-icon >mdi-bell</v-icon>
-                  </v-badge>
-                 </v-btn>
-                </template>
-                <v-list>
-                <v-list-item
-                >
-                    <v-list-item-title>Notifications</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-menu>
+              <v-menu offset-y
+                
+              >
+                  <template v-slot:activator="{ on, attrs }">
+                   <v-btn
+                   plain
+                   v-bind="attrs"
+                   v-on="on"
+                   @click="chagerEtatNotif"
+                   >
+                      <v-icon size="35px">mdi-bell</v-icon>
+                      <v-badge color="red" :content="getNbrNotifNotSeen"
+                      v-if="getNbrNotifNotSeen!=0"
+                      :values="getNbrNotifNotSeen"
+                      ></v-badge>
+                   </v-btn>
+                   
+                  </template>
+                  <v-list v-if="notifications.length==0">
+                  <v-list-item  class="mt-5">
+                      <v-list-item-title>No notifications !</v-list-item-title>
+                  </v-list-item>
+                 </v-list>
+                  <v-list v-else>
+                  <v-list-item
+                      v-for="notif in notifications" :key="notif.id"
+                  >
+                      <v-list-item-title>{{ notif.msg }}</v-list-item-title>
+                      </v-list-item>
+                      <v-list-item class="mt-5">
+                          <v-btn 
+                          plain
+                          v-if="notifications.length!=0"
+                          @click="deleteAllNotif()"
+                          >
+                              <v-list-item-title class="red--text"> 
+                                  
+                                  Clear All<v-icon class="pa-1 red--text">mdi-delete</v-icon>
+                              </v-list-item-title>
+                          </v-btn>
+                      </v-list-item>
+                  </v-list>
+              </v-menu>
               <v-menu offset-y>
                 <template v-slot:activator="{ on, attrs }">
                  <v-btn
@@ -54,16 +78,13 @@
                     <v-list-item-title>{{store.user['FirstName']}}</v-list-item-title>
                     </v-list-item>
                 </v-list>
-
                 <v-list-item>
-
                   <v-list-item-action>
                       <v-btn plain @click="logout()">
                           <v-icon class="pa-2">mdi-logout</v-icon>
                           <span class="">log out</span>
                       </v-btn>
                   </v-list-item-action>
-
               </v-list-item>
             </v-menu>
               </v-row>
@@ -72,20 +93,66 @@
 </template>
 
 <script>
+import gererNotifEtud from "@/service/NotifEtudiant/gererNotifEtud"
 import {AuthUser} from "@/store/Store.js";
 import {CurentView} from "@/store/StoreView.js";
 export default {
+  created(){
+            this.getNotifs();
+            this.getNotifsNotSeen();
+         },
   setup(){
     const store = AuthUser();
      const store_view = CurentView();
      return {store,store_view};
   },
+  data(){
+    return{
+          notifications:[],
+          notificationNotSeen:[],
+    }
+  },
   methods:{
-    logout(){
+       logout(){
+            this.store_view.suppView();
             this.store.logout();
             this.$router.push({name:'signin'});
-        }
-  }
+        },
+        getNotifs(){
+            gererNotifEtud.getNotifEtud().then((res)=>{
+                for(let i=0;i<(res.data.data).length;i++){
+                    this.notifications.push({idNotif:res.data.data[i].id,msg:res.data.data[i].message,etat:res.data.data[i].etat})
+                }
+            }
+            )
+        },
+        deleteAllNotif(){
+            gererNotifEtud.deleteAllNotif().then((res)=>{
+                this.notifications=[];
+                this.notificationNotSeen=[];
+            })
+        },
+        chagerEtatNotif(){
+            gererNotifEtud.updateNotif();
+        },
+        getNotifsNotSeen(){
+            gererNotifEtud.getNotifNotSeen().then((res)=>{
+                for(let i=0;i<(res.data.data).length;i++){
+                    this.notificationNotSeen.push({idNotif:res.data.data[i].id,msg:res.data.data[i].message,etat:res.data.data[i].etat})
+                }
+            })
+         },
+  },
+  computed:{
+            getNbrNotif(){
+                const nbrNotif=this.notifications.length
+                return nbrNotif;
+            },
+            getNbrNotifNotSeen(){
+                    const nbrNotifNotSeen=this.notificationNotSeen.length
+                    return nbrNotifNotSeen;
+            }
+         }   
 };
 
 </script>
