33789864f3d5cfdeed373b1647b334a4f7b71597
talel <talelmejri8@gmail.com>
2023-05-01 00:39:14 +0100

    add function SendEmailChanger in User Model

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use App\Notifications\VerifyEmail;
+use App\Notifications\EditProfil\SendChangerEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -45,6 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function SendEmailChanger($email){
+        $this->notify(new SendChangerEmail($email));
+    }
 
     public function roles(){
         return $this->belongsToMany(Role::class,"role_users")->withPivot(['status']);
