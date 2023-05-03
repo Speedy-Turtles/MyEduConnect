35a0584a31413ee60b95897b45b5dcba4c0e004c
hkimi amin <hkimiamin02@gmail.com>
2023-05-03 17:51:09 +0100

    add notif chef_dep

--- vue/src/components/chef_departement/navbar.vue
+++ vue/src/components/chef_departement/navbar.vue
@@ -5,17 +5,32 @@
             <v-spacer></v-spacer>
             <v-menu offset-y>
                 <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon v-bind="attrs" v-on="on" style="margin-right: 2%;">
-                        <v-badge color="red" content="6">
+                    <v-btn icon v-bind="attrs" v-on="on" style="margin-right: 2%;" @click="changeEtatNotif()">
+                        <v-badge color="red" :content="lengthNotseenNotif">
                             <v-icon>mdi-bell</v-icon>
                         </v-badge>
                     </v-btn>
                 </template>
                 <v-list>
                     <v-list-item-title>
-                        <v-list-item class="notification">notifications 1</v-list-item>
-                        <v-list-item class="notification">notifications 2</v-list-item>
-                        <v-list-item class="notification">notifications 3</v-list-item>
+                        <v-list-item v-if="notifications.length==0">
+                            no notifications !
+                        </v-list-item>
+                        <v-list-item class="notification" v-if="notifications.length!=0" v-for="notif in notifications" :key="notif.id" :class="[notif.etat==0 ? 'notseen' : '' ]">
+                            {{ notif.message }}
+                        </v-list-item>
+                        <v-list-item class="mt-5">
+                            <v-btn 
+                            plain
+                            v-if="notifications.length!=0"
+                            @click="deleteAllNotif()"
+                            >
+                                <v-list-item-title class="red--text"> 
+                                    
+                                    Clear All<v-icon class="pa-1 red--text">mdi-delete</v-icon>
+                                </v-list-item-title>
+                            </v-btn>
+                        </v-list-item>
                     </v-list-item-title>
                 </v-list>
             </v-menu>
@@ -68,7 +83,6 @@
                     </v-list-item>
                     
                     <v-list-item>
-
                         <v-list-item-action>
                             <v-btn plain @click="logout()">
                                 <v-icon class="pa-2">mdi-logout</v-icon>
@@ -88,22 +102,59 @@
 </template>
 <script>
 import {AuthUser} from "@/store/Store.js"
+import gererNotifEtud from "@/service/NotifEtudiant/gererNotifEtud";
 export default {
     name: 'navbar',
+    created(){
+        this.getNotif();
+        this.getNotifNotSenn();
+        console.log(this.notifications);
+    },
     setup(){
             const store=AuthUser();
             return{store}
         },
     data() {
         return {
-
+            notifications:[],
+            notSeenNotification:[],
         }
     },
     methods:{
         logout(){
             this.store.logout();
             this.$router.push({name:'signin'});
-        }
+        },
+        getNotif(){
+            gererNotifEtud.getNotifEtud().then(response=>{
+                console.log(response.data.data);
+                this.notifications=response.data.data;
+                console.log(this.notifications);
+            })
+        },
+        getNotifNotSenn(){
+            gererNotifEtud.getNotifNotSeen().then(response=>{
+                this.notSeenNotification=response.data.data;
+                console.log(this.notSeenNotification);
+            })
+        },
+        changeEtatNotif(){
+            gererNotifEtud.updateNotif().then(response=>{
+                this.notSeenNotification=[];
+                this.getNotif();
+            });
+        },
+        deleteAllNotif(){
+            gererNotifEtud.deleteAllNotif().then((res)=>{
+                this.notifications=[];
+                this.notificationNotSeen=[];
+            })
+        },
+        },
+        computed:{
+            lengthNotseenNotif(){
+                return this.notSeenNotification.length > 0 ? this.notSeenNotification.length : 0;
+            }
         }
 }
 </script>
@@ -116,5 +167,8 @@ export default {
 
 .notification {
     cursor: pointer;
+}
+.notseen{
+    background: lightblue!important;
 }
 </style>
