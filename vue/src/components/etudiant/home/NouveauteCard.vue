e83dc2a49508e1fafeb349eee86b2bfc9a65d03c
ghassen <ghassenahmed74@gmail.com>
2023-05-03 02:17:03 +0100

    affichage neauvelty

--- vue/src/components/etudiant/home/NouveauteCard.vue
+++ vue/src/components/etudiant/home/NouveauteCard.vue
@@ -13,36 +13,72 @@
                     <v-layout row>
                         <v-flex class="pa-5"><h1>Novelty</h1></v-flex>
                     </v-layout>
-                    <div id="card" v-for="i in 5" data-aos="fade-down" :key="i">
-                        <v-layout row wrap >
-                            <v-flex xl4 md3 sm4 xs12 class=""><img src="../../../../public/etudiant/images/p1.png" width="150px" alt="" srcset=""></v-flex>
+                    <div class="NoNeauvelty ma-10 " v-if="neavautes.length==0">
+                        <span class="msgErr">No Neauvelty is availble</span>
+                    </div>
+                    <div id="card"  data-aos="fade-down" v-else>
+                        <div class="Neauvelty" v-for="neauv in neavautes" :key="neauv.id">
+                            <v-layout row wrap >
+                            <v-flex xl4 md3 sm4 xs12 class=""> <img :src="neauv.photo" width="150px" alt="" srcset=""></v-flex>
                             <v-flex xl8 md9 sm8 xs12 class="card_title">
                                 <v-layout>
                                     <v-flex>
-                                        <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, delectus?</h5>
+                                        <h5>{{ neauv.titre }}</h5>
                                     </v-flex> 
                                 </v-layout>
                                 <v-layout>
                                     <v-flex class="card_date">
-                                        <span >25/08/2023</span>
+                                        <span >{{neauv.created_at}}</span>
                                     </v-flex>
                                 </v-layout>
                                     
                             </v-flex>
                         </v-layout>
                         <v-layout row>
-                            <v-flex class="card_desc"><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt modi doloribus magnam expedita voluptatem placeat blanditiis cum. Debitis, culpa in.</p></v-flex>
+                            <v-flex class="card_desc"><p>{{ neauv.descripition }}</p></v-flex>
                         </v-layout>
+                        </div>
+                        
                     </div>
-                    <v-btn class="float-end" plain>Suivant<v-icon>mdi-arrow-right</v-icon></v-btn>
+                    <v-layout row >
+                        <v-flex v-if="neavautes.length>0">
+                            <v-btn class="float-end" @click="this.page < this.totalPages ? this.pages++: ''" plain>Next<v-icon>mdi-arrow-right</v-icon></v-btn> 
+                            <v-btn class="float-end"  @click="page--" plain><v-icon>mdi-arrow-left</v-icon>Previous</v-btn>
+                               
+                        </v-flex>                     
+                    </v-layout>
+                    
             </v-flex>
         </v-layout>
 
     </div>
 </template>
 <script>
+import gererNeavaute from '@/service/neavaute/gererNeavaute';   
 export default {
-
+data(){
+    return{
+        neavautes: [],
+        page: 1,
+        totalPages: 0,
+    }
+},
+methods:{
+    getNeavaute() {
+            this.loader = true;
+            gererNeavaute.getNeavaute(this.page).then(response => {
+                this.neavautes = response.data.data.data;
+                console.log(response.data.data);
+                this.totalPages = response.data.data.last_page;
+                this.loader = false;
+            })
+        },
+       
+    
+}, created() {
+        
+        this.getNeavaute();
+    },
 }
 </script>
 <style scoped>
@@ -67,5 +103,10 @@ export default {
     padding-top: 37px;
     font-size: 15px !important;
     font-weight: lighter !important;
+}
+.msgErr{
+    text-align: center;
+    font-size: 30px;
+    color: red;
 }
 </style>
