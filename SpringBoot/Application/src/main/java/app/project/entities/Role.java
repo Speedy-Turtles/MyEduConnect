94349d452c03da7fce1021a5856e76dfd9226c57
talel <talelmejri8@gmail.com>
2023-05-07 01:54:04 +0100

    add role entity

---
+++ SpringBoot/Application/src/main/java/app/project/entities/Role.java
@@ -0,0 +1,38 @@
+package app.project.entities;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@AllArgsConstructor@NoArgsConstructor@Data
+
+public class Role implements Serializable {
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+@Id
+ @GeneratedValue(strategy = GenerationType.AUTO)
+ private long id;
+ 
+ private String RoleName;
+ 
+    @CreationTimestamp
+	private Timestamp created_at;
+	
+	@UpdateTimestamp
+	private Timestamp updated_at;
+ 
+}
