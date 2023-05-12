2b55837c004169aadb1aeb6661bf15661c31d39d
talel <talelmejri8@gmail.com>
2023-05-12 16:57:35 +0100

    fix bugs

--- SpringBoot/Application/src/main/java/app/project/repository/RoleRepository.java
+++ SpringBoot/Application/src/main/java/app/project/repository/RoleRepository.java
@@ -1,11 +1,17 @@
 
 package app.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import app.project.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
- 
+	@Query(value="select * from role where id=:id",nativeQuery=true)
+	Role getRoleById(long id);
+	
+	List<Role> findAll();
 }
 
