fcdf9b463aebe0d589851370ba9e2ec4ba99b3fd
talel <talelmejri8@gmail.com>
2023-05-16 00:50:46 +0100

    affiche Specialite,pagination,search,per_page

--- frontspring/src/components/Admin/Specialite/GererSpecialite.vue
+++ frontspring/src/components/Admin/Specialite/GererSpecialite.vue
@@ -13,6 +13,16 @@
                             hide-details
                        ></v-text-field>  
                     </div>
+                    
+                      <div class="col-lg-1 mt-4 mx-2 text-center">
+                        <v-select
+                            v-model="per_page"
+                            :items="items"
+                            @change="FetchData()"
+                            outlined
+                            dense
+                      ></v-select>
+                    </div>
                       <v-spacer></v-spacer>
                       <div class="col-lg-3  text-center">
                         <div class="row">
@@ -41,46 +51,66 @@
           <template v-slot:default>
             <thead>
               <tr>
-                <th  class="text-left ">
+                <th  class="text-center ">
                   Id
                 </th>
-                <th  class="text-left ">
-                  Nom
-                </th>
-                <th  class="text-left ">
+                <th  class="text-center ">
                   Niveau
                 </th>
-                <th class="text-left">
+                <th  class="text-center ">
+                  Type
+                </th>
+                <th class="text-center">
                   Operation
                 </th>
               </tr>
             </thead>
-           
-            <tbody >
-                
+            <tbody v-if="spec==''">
+                <tr>
+                   <td colspan="5" class="text-center">No data</td>
+                </tr>
             </tbody>
-            <!-- <div class="text-center mt-2">
-              <v-btn small :disabled="pagination.prevpage==null"  @click="changerPage(pagination.curentpage-1)">
+            <tbody v-else>
+                <tr v-for="val in spec" :key="val.id">
+                   <td class="text-center">{{val.idspec}}</td>
+                   <td class="text-center">{{val.niveau}}</td>
+                   <td class="text-center">{{val.type}}</td>
+                   <td class="text-center">
+                       <v-btn color="error"  class="mx-2">
+                          <v-icon>
+                            mdi-trash-can
+                          </v-icon>
+                       </v-btn>
+                       <v-btn  color="warning" class="mx-2 ">
+                          <v-icon>
+                            mdi-circle-edit-outline
+                          </v-icon>
+                       </v-btn>
+                   </td>
+                </tr>
+            </tbody>
+             <div class="text-center mt-2">
+              <v-btn small :disabled="testPrev==true"  @click="changerPage(pageCurrent-1 )">
                 <v-icon>
                   mdi-chevron-left
                </v-icon>
-             </v-btn>
-             <v-btn v-for="num in (Math.ceil(pagination.per_page ? pagination.total/pagination.per_page : 1))" :key="num"
-               @click="changerPage(num)"
+             </v-btn> 
+             <v-btn v-for="num in countPage" :key="num"
+                @click="changerPage(num)"
                 class="mx-2"
                 color="primary"
-                :disabled="num==pagination.curentpage"
+                :disabled="num==pageCurrent"
                 small
                 x-small
               >
              {{num}}
            </v-btn>
-              <v-btn small :disabled="pagination.nextpage==null"  @click="changerPage(pagination.curentpage+1)">
+              <v-btn small :disabled="testNext==true"  @click="changerPage(pageCurrent+1)">
                  <v-icon>
                     mdi-chevron-right
                  </v-icon>
               </v-btn>
-            </div> -->
+            </div> 
           </template>  
           
         </v-simple-table>
@@ -89,15 +119,41 @@
 </template>
 
 <script>
+import serviceSpec from "@/service/GereSpecialite/gereSpec.js"
 export default{
     name:'spec',
+    created(){
+      this.FetchData();
+    },
     data() {
         return {
-            
+          spec:[],
+          search:"",
+          countPage:[],
+          per_page:2,
+          items:[2,5,10,15],
+          pageCurrent:0,
+          testNext:true,
+          testPrev:true
         }
     },
     methods: {
-        
+      FetchData(){
+        serviceSpec.chercherSpec(this.pageCurrent,this.search,this.per_page).then((res)=>{
+          this.countPage=res.data.count_page;
+          this.spec=res.data.spec.content;
+          this.pageCurrent=res.data.page;
+          this.testNext=res.data.spec.last;
+          this.testPrev=res.data.spec.first;
+        })
+      },
+      changerPage(num){
+        if(num<0){
+          num=0;
+        }
+        this.pageCurrent=num;
+        this.FetchData();
+      }
     },
 }
 </script>
