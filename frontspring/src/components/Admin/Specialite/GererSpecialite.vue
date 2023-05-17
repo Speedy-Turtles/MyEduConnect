7586a989f45ae7d735ce92d44e4f5d42256aae6d
talel <talelmejri8@gmail.com>
2023-05-17 18:21:48 +0100

    Gerer Specialte

--- frontspring/src/components/Admin/Specialite/GererSpecialite.vue
+++ frontspring/src/components/Admin/Specialite/GererSpecialite.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
+  <div v-if="updateSpec && !AddSepc ">
+    <UpdateSpecVue
+                    :Spec="SelectUpdateVal"
+                    @close_update="close_update"
+                    :updateSpec="updateSpec"
+    ></UpdateSpecVue>
+  </div >
+  <div  v-else-if="!updateSpec && AddSepc ">
+    <AddSpecialite
+    @close_update="close_update"
+    :updateSpec="updateSpec"
+></AddSpecialite>
+  </div>
+    <div v-else>
         <v-card elavation="7" style="padding:25px">
             <h4 class="text-center py-4">Specialite</h4>
                   <div class="row gap-4 p-5 mt-3">
@@ -27,10 +40,10 @@
                       <div class="col-lg-3  text-center">
                         <div class="row">
                           <div class="col-lg-12 text-center">
-                            <v-tooltip bottom>
+                            <v-tooltip bottom  >
                               <template v-slot:activator="{ on, attrs }">
                                   <v-btn
-                                  @click="AddCategory=true"
+                                  @click="AddSepc=true"
                                   class="mb-5"
                                   v-bind="attrs"
                                   v-on="on"
@@ -41,7 +54,7 @@
                                   <v-icon>mdi-book-education</v-icon>
                                 </v-btn>
                               </template>
-                              <span>Ajouter Specialte</span>
+                              <span>Add Specialte</span>
                             </v-tooltip>
                       </div>
                           </div> 
@@ -76,16 +89,30 @@
                    <td class="text-center">{{val.niveau}}</td>
                    <td class="text-center">{{val.type}}</td>
                    <td class="text-center">
-                       <v-btn color="error"  class="mx-2">
+                       <v-btn color="error" @click="deleteSpec(val)"  class="mx-2">
                           <v-icon>
                             mdi-trash-can
                           </v-icon>
                        </v-btn>
-                       <v-btn  color="warning" class="mx-2 ">
+                       <v-btn @click="UpdateSpec(val)"  color="warning" class="mx-2 ">
                           <v-icon>
                             mdi-circle-edit-outline
                           </v-icon>
                        </v-btn>
+                       <v-tooltip bottom  >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                              v-bind="attrs"
+                              v-on="on"
+                              @click="ShowAddClass(val)"
+                              style="color:#fff !important"
+                              color="blue"
+                            >
+                            <v-icon>mdi-plus</v-icon>
+                          </v-btn>
+                        </template>
+                        <span>Add Classe</span>
+                      </v-tooltip>
                    </td>
                 </tr>
             </tbody>
@@ -115,10 +142,92 @@
           
         </v-simple-table>
       </v-card>
+      <v-dialog  v-if="IdSpecSelected!=0"
+      transition="dialog-bottom-transition"
+      max-width="600"
+      v-model="dialogAddclasse"
+    >
+     <template >
+       <v-card>
+         <v-toolbar
+           color="blue"
+           dark
+         >Add Classe </v-toolbar>
+         <v-card-text>
+          <v-text-field
+              name="Name"
+              v-model="classe_name"
+              required
+              label="Name"
+              type="text"
+              placeholder="Enter Name"
+            ></v-text-field>
+         </v-card-text>
+         <v-card-actions class="justify-end">
+           <v-btn 
+            :loading="load"
+             text
+              @click="AddClasse()"
+           >Add</v-btn>
+           <v-btn
+           text
+           @click="dialogAddclasse=false"
+         >fermer</v-btn>
+         </v-card-actions>
+       </v-card>
+     </template>
+   </v-dialog> 
+
+      <v-dialog  v-if="item_selected!=''"
+      transition="dialog-bottom-transition"
+      max-width="600"
+      v-model="dialog"
+    >
+     <template >
+       <v-card>
+         <v-toolbar
+           color="danger"
+           dark
+         >Supprimer Specialte </v-toolbar>
+         <v-card-text>
+           <div class="text-h2 pa-12">{{item_selected.type}} Niveau {{item_selected.niveau}}</div>
+         </v-card-text>
+         <v-card-actions class="justify-end">
+           <v-btn 
+             text
+             :loading="load"
+              @click="confrimDelete(item_selected.idspec)"
+           >Supprimer</v-btn>
+           <v-btn
+           text
+           @click="dialog=false"
+         >fermer</v-btn>
+         </v-card-actions>
+       </v-card>
+     </template>
+   </v-dialog> 
+   
+   <v-snackbar
+   v-model="snackbar"
+ >
+    {{ message }}
+   <template v-slot:action="{ attrs }">
+     <v-btn
+       color="indigo"
+       text
+       v-bind="attrs"
+       @click="snackbar = false"
+     >
+       Fermer
+     </v-btn>
+   </template>
+ </v-snackbar>
     </div>
 </template>
 
 <script>
+import AddSpecialite from "@/components/Admin/Specialite/AddSpec.vue"
+import UpdateSpecVue from "@/components/Admin/Specialite/UpdateSpec.vue"
 import serviceSpec from "@/service/GereSpecialite/gereSpec.js"
 export default{
     name:'spec',
@@ -129,15 +238,53 @@ export default{
         return {
           spec:[],
           search:"",
+          message:"",
+          snackbar:false,
+          dialogAddclasse:false,
+          IdSpecSelected:0,
+          SelectUpdateVal:[],
           countPage:[],
+          item_selected:[],
+          updateSpec:false,
           per_page:2,
           items:[2,5,10,15],
           pageCurrent:0,
+          load:false,
+          classe_name:"",
           testNext:true,
-          testPrev:true
+          AddSepc:false,
+          testPrev:true,
+          dialog:false
         }
     },
     methods: {
+      ShowAddClass(val){
+        this.dialogAddclasse=true;
+        this.IdSpecSelected=val.idspec;
+      },
+      AddClasse(){
+        this.load=true;
+        serviceSpec.AddClasse({
+          "name":this.classe_name,
+          "id":this.IdSpecSelected
+        }).then((res)=>{
+          this.load=false;
+           this.dialogAddclasse=false;
+           this.IdSpecSelected=0;
+           this.FetchData();
+           this.message="Classe Added";
+          this.snackbar=true;
+        })
+      },
+      close_update(){
+        this.updateSpec=false;
+        this.AddSepc=false;
+        this.FetchData();
+      },
+      UpdateSpec(val){
+        this.SelectUpdateVal=val;
+        this.updateSpec=true;
+      },
       FetchData(){
         serviceSpec.chercherSpec(this.pageCurrent,this.search,this.per_page).then((res)=>{
           this.countPage=res.data.count_page;
@@ -153,7 +300,30 @@ export default{
         }
         this.pageCurrent=num;
         this.FetchData();
+      },
+      deleteSpec(val){
+        this.item_selected=val;
+        this.dialog=true;
+      },
+      confrimDelete(id){
+        this.load=true;
+        serviceSpec.deleteSpec(id).then((res)=>{
+          if(res.data==true){
+            this.load=false;
+            this.item_selected=[];
+            this.message="Delete speicialte with success";
+            this.snackbar=true;
+            this.dialog=false;
+            this.FetchData();
+          }else{
+            
+          }
+        
+        })
       }
     },
+    components:{
+      UpdateSpecVue,AddSpecialite
+    }
 }
 </script>
