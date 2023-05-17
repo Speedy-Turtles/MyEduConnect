f87dd37adcdd41120799db969b1ff80fcad48873
talel <talelmejri8@gmail.com>
2023-05-17 18:22:06 +0100

    add functions In GererUser

--- frontspring/src/service/GereSpecialite/gereSpec.js
+++ frontspring/src/service/GereSpecialite/gereSpec.js
@@ -6,5 +6,17 @@ export default{
     async chercherSpec(page,search,per){
         const res= await Axios.get(`getSepecialite?${search ? "search="+search : ""}&page=${page}&per_page=${per}`);
         return res;
+    },
+    deleteSpec(id){
+        return Axios.delete("deleteSpec?id="+id);
+    },
+    updateSpec(data){
+        return Axios.post("updateSpecilte",data);
+    },
+    AddSpec(data){
+        return Axios.post("AddSpec",data);
+    },
+    AddClasse(classe){
+        return Axios.post("AddClasse",classe);
     }
 }
