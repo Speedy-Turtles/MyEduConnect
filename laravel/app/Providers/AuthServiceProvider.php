4e2a020f1906f8bf3f9224bb62cb816f4248f69b
talel <talelmejri8@gmail.com>
2023-05-23 22:56:32 +0100

    applique policies

--- laravel/app/Providers/AuthServiceProvider.php
+++ laravel/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,13 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Suggestion;
+use App\Models\User;
+use App\Policies\SuggestionPolicy;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Suggestion::class=>SuggestionPolicy::class,
     ];
 
     /**
@@ -21,6 +28,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+       /* Gate::define('admin',function(User $user){
+            return $user->roles->contains("Role_name","Admin");
+        });*/
     }
 }
