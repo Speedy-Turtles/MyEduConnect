09c7f4c84583b28e0463b8ca873d6642db69bfcc
talel <talelmejri8@gmail.com>
2023-05-23 22:55:50 +0100

    add mfs suggestion

---
+++ laravel/app/Models/Suggestion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Suggestion extends Model
+{
+    use HasFactory;
+}
