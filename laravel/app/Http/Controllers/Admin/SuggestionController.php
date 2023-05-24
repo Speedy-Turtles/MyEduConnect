8c8eac78c0870d6aa0534fc6b931d3ff95e107c6
talel <talelmejri8@gmail.com>
2023-05-24 16:42:32 +0100

    delete_all_sugg,Add notif when add sugg,filter

--- laravel/app/Http/Controllers/Admin/SuggestionController.php
+++ laravel/app/Http/Controllers/Admin/SuggestionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Events\notif;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSuggestion;
+use App\Models\Notification;
 use App\Models\Suggestion;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
@@ -13,14 +16,22 @@ class SuggestionController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Suggestion::class,'Suggestion');
+        $this->authorizeResource(Suggestion::class,'suggestion');
     }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-            $all_sugg=Suggestion::paginate(3);
+            if($request->search=='' && $request->per_page==''){
+                $all_sugg=Suggestion::with("user")->paginate(3);
+            }else if($request->search!='' && $request->per_page==''){
+                $all_sugg=Suggestion::with("user")->where('type',$request->search)->paginate(3);
+            }else if($request->search=='' && $request->per_page!=''){
+                $all_sugg=Suggestion::with("user")->paginate($request->per_page);
+            }else if($request->search!='' && $request->per_page!=''){
+                $all_sugg=Suggestion::with("user")->where('type',$request->search)->paginate($request->per_page);
+            }
             return response()->json(['data'=>$all_sugg],200);
     }
 
@@ -34,7 +45,16 @@ class SuggestionController extends Controller
          $sugg->type=$request->type;
          $sugg->Message=$request->message;
          $sugg->save();
-         return response()->json(['data'=>$sugg],201);
+         $admin=User::whereHas('roles',function($query){
+            $query->where("Role_name","Admin");
+        })->first();
+        $notif=new Notification();
+        $notif->id_envoi=$request->user()->id;
+        $notif->id_recu=$admin->id;
+        $notif->message=$request->user()->FirstName .' add a suggestion';
+        $notif->save();
+        broadcast(new notif($notif->message));
+        return response()->json(['data'=>$sugg],201);
     }
 
     /**
@@ -56,14 +76,19 @@ class SuggestionController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Suggestion $suggestion)
+    public function delete(int $id)
     {
-       $sugg=Suggestion::find($suggestion->id);
+       $sugg=Suggestion::find($id);
        if($sugg){
             $sugg->delete();
             return response()->json(['data'=>"delete with success"],200);
        }else{
         return response()->json(['data'=>"Not Found"],404);
        }
+    }
+
+    public function delete_all_sugg(){
+        Suggestion::query()->delete();
+        return response()->json(['data' => 'Delete All'], 200);
     }
 }
